256e50eeaa0fdf55f44c0e599634893782019fc0
Ron Liebregts <ronlieb@hotmail.com>
2021-09-22 13:29:32 +0200

    Dropdown list
    voor tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/task-list.ts
@@ -0,0 +1,6 @@
+import { Task } from './task';
+
+export const tasks: Task[] = [
+  { id: 1, title: 'Visit the breakroom', description: 'Get coffee' },
+  { id: 2, title: 'Meet colleagues', description: 'Flirt witht he receptionist' }
+];
