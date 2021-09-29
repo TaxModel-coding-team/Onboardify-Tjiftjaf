ee212dd536ea4338744267721ef958581390701d
Ron Liebregts <ronlieb@hotmail.com>
2021-09-29 10:38:15 +0200

    Accordion fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
--- src/app/task-list.ts
+++ src/app/task-list.ts
@@ -1,8 +1,8 @@
 import { Task } from './task';
 
 export const tasks: Task[] = [
-  { id: 1, name: 'Visit the breakroom', description: 'Get coffee', exp: 200 },
-  { id: 2, name: 'Meet colleagues', description: 'Flirt witht he receptionist', exp: 500 },
-  { id: 2, name: 'Do a backflip', description: 'do a blackflip', exp: 200 },
-  { id: 2, name: 'yeeeeeeet', description: 'yeeeeeeet', exp: 200 }
+  { id: 1, name: 'Visitthebreakroom', description: 'Get coffee', exp: 200 },
+  { id: 2, name: 'Meetcolleagues', description: 'Flirt witht he receptionist', exp: 500 },
+  { id: 3, name: 'Doabackflip', description: 'do a blackflip', exp: 200 },
+  { id: 4, name: 'Bier', description: 'yeeeeeeet', exp: 200 }
 ];
