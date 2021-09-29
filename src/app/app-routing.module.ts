6e5360025d031077ee0a602a59da335c562cf0d0
maxxer789 <maxvdd76@gmail.com>
2021-09-29 11:13:01 +0200

    Revert "Merge branch 'task-list' into main"
    This reverts commit 46183ac31a02b14aad462fe6dd8a6c074cb1991a, reversing
    changes made to 71f3c1b053cb746c0009fe65fbe03e8a1ed24ab2.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/task-list.ts
+++
@@ -1,8 +0,0 @@
-import { Task } from './task';
-
-export const tasks: Task[] = [
-  { id: 1, name: 'Visitthebreakroom', description: 'Get coffee', exp: 200 },
-  { id: 2, name: 'Meetcolleagues', description: 'Flirt witht he receptionist', exp: 500 },
-  { id: 3, name: 'Doabackflip', description: 'do a blackflip', exp: 200 },
-  { id: 4, name: 'Bier', description: 'yeeeeeeet', exp: 200 }
-];
