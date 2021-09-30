ed6947542e0d1147487d835fb3609cac3630d70c
Ron Liebregts <ronlieb@hotmail.com>
2021-09-30 13:20:03 +0200

    skrrt

---
+++ src/app/tasks/quests.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Quest } from '../quest';
+import { QuestService } from '../quest.service';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class QuestsComponent implements OnInit {
+
+  quests: Quest[] = [];
+
+  constructor(private taskService: QuestService) { }
+
+  ngOnInit(): void {
+    this.getQuests();
+  }
+
+  getQuests(): void {
+    this.taskService.getQuests()
+      .subscribe(quest => this.quests = quest);
+  }
+
+  getGreeting(): String{
+    var greeting: String;
+    var today = new Date()
+    var curHr = today.getHours()
+
+    if (curHr < 12) {
+      greeting = 'Good morning';
+    } else if (curHr < 18) {
+      greeting = 'Good afternoon';
+    } else {
+      greeting = 'Good evening';
+    }
+    return greeting;
+  }
+
+
+}
