a05d9537d8efb0bcb58eb81761ee074d29a3d6c2
Jitske-Bots <58811242+Jitske-Bots@users.noreply.github.com>
2021-11-24 14:50:27 +0100

    fixed login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AchievementsComponent } from './achievements/achievements.component';
+import { MicrosoftLoginComponent } from './microsoft-login/microsoft-login.component';
+import { RegistrationComponent } from './Registration/registration.component';
+import { QuestsComponent } from './quests/quests.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MicrosoftLoginComponent, pathMatch: 'full'},
+  { path: 'quests', component: QuestsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
