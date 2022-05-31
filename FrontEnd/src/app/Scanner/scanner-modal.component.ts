1ee3df3da8645417a56ca24001943599855b5547
Dennis-APPELS <60885395+Dennis-APPELS@users.noreply.github.com>
2022-05-31 14:24:10 +0200

    Update scanner-modal.component.ts

--- FrontEnd/src/app/Scanner/scanner-modal.component.ts
+++ FrontEnd/src/app/Scanner/scanner-modal.component.ts
@@ -2,16 +2,11 @@ import {Component, Inject, OnInit} from "@angular/core";
 import {CookieService} from "ngx-cookie-service";
 import {Router} from "@angular/router";
 import {UserService} from "../Services/user.service";
-import { PopUpCompleteComponent } from "./pop-up-complete.component";
-import { PopUpGetQuestComponent } from "./pop-up-getquest.component"
 import { MatDialogRef } from "@angular/material/dialog";
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { QuestService } from "../Services/quest.service";
 import { questUserViewModel } from "../Models/questUserViewModel";
-import { MatDialog } from  '@angular/material/dialog';
-import { PopUpError } from "./Pop-up-error";
-import { PopupClient } from "@azure/msal-browser";
-import { Console } from "console";
+
 
 @Component({
   selector: 'app-scanner-modal',
@@ -19,24 +14,18 @@ import { Console } from "console";
   styleUrls: ['./scanner-modal.component.css']
 })
 
-export class ScannerModalComponent implements OnInit {
-
+export class ScannerModalComponent {
   constructor(private cookieService: CookieService,
               private userService: UserService,
               private questService: QuestService,
               public dialogRef: MatDialogRef<ScannerModalComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: any, private dialog: MatDialog) {   }
+              @Inject(MAT_DIALOG_DATA) public data: any) {  }
   public currentDevice: MediaDeviceInfo | undefined;
+  //public popupComplete: PopUpComplete,
   public availableCameras: MediaDeviceInfo[] = [];
   public hasDevices: boolean = false;
   public returnedvalue: boolean = false;
-  public popupComplete: PopUpCompleteComponent = {} as PopUpCompleteComponent;
-  public popupGetQuest: PopUpGetQuestComponent = {} as PopUpGetQuestComponent;
   private questUserViewModel : questUserViewModel = {} as questUserViewModel;
-
-  ngOnInit(): void {
-    
-  }
 
   camerasFound(cameras: MediaDeviceInfo[]){
     this.availableCameras = cameras;
@@ -44,43 +33,33 @@ export class ScannerModalComponent implements OnInit {
     this.currentDevice = this.availableCameras[0];
   }
 
-  async onScanSuccess(result: string){
-    this.dialogRef.close();
+  onScanSuccess(result: string){
     let userId = JSON.parse(this.cookieService.get("user")).id
     if(result.includes("complete") == true)
     {
       this.questUserViewModel.UserId = userId;
       this.questUserViewModel.QuestId = result.slice(0,-9);
-      var Result : Boolean;
-      Result = await this.questService.completeQuest(this.questUserViewModel);
-      if(Result == true)
+      if(this.questService.completeQuest(this.questUserViewModel) == true)
       {
-        this.dialog.open(PopUpCompleteComponent , {
-          data: {
-            QuestTitle : localStorage.getItem("QuestTitle")},
-        });
+       // this.PopUpComplete.open();
       } else{
-        this.dialog.open(PopUpError, { panelClass: '.e-dialog'});
+        //this.popupComplete.open(false)
       }
     } else{
       this.questUserViewModel.UserId = userId;
       this.questUserViewModel.QuestId = result.slice(1,-1);
-      if(await this.questService.assignQuestByQR(this.questUserViewModel) == true)
+      if(this.questService.assignQuestByQR(this.questUserViewModel) == true)
       {
-        this.dialog.open(PopUpGetQuestComponent, {
-          data: { QuestTitle : localStorage.getItem("QuestTitle")},
-        });
+        //this.popupGetQuest.open(true)
       }else{
-        this.dialog.open(PopUpError, { panelClass: '.e-dialog'});
+        //this.popupGetQuest.open(false)
       }
     } 
+   
   }
 
   closeDialog() {
-    localStorage.clear();
     this.dialogRef.close();
   }
-
-  
 
 }
